8b8f21b611de6b1242d130a24f093b28edd8984c
geet1987 <52599381+geet1987@users.noreply.github.com>
2022-11-27 21:05:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,25 @@
-import {useState} from 'react';
+// import React from 'react'
+// import '../styles/App.css';
+// const App = () => {
+//   const [count, setCount] = useState(true);
+//   return (
+//     <div id="main">
+//       <h1 id='marco polo'>{setCount}</h1>
+//     </div>
+//   )
+// }
+
+
+// export default App;
+import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
-  const [titles,setTitles]=useState({
-  button:"polo",
-    h1:"Marco",
-  });
-  const swap=()=>{
-  setTitles((prevState)=>{
-    return{
-      button:prevState.h1,
-      h1:prevState.button
-    };
-  });
-  };
-  
-  
+  const [st, setSt] = useState(true);
+
   return (
     <div id="main">
-<h1 id= "marco-polo">{titles.h1}</h1>
-    <button onClick={swap}id="marco-polo-toggler">{titles.button}</button>
-    
+      <h1 id="marco-polo">{st ? 'Marco' : 'Polo'}</h1>
+      <button id="marco-polo-toggler" onClick={() => setSt(st => !st)}>{st ? 'Polo' : 'Marco'}</button>
     </div>
   )
 }
